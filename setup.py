7b3951287df5494ef28f8c38675b1ca62de345ff
Thomas Bechtold <t.bechtold@telekom.de>
2013-11-19 15:15:18 +0100

    Use Apache-2.0 instead of GPL-3
    Relicense the software to Apache-2.0.

--- setup.py
+++ setup.py
@@ -1,4 +1,19 @@
 # -*- coding: utf-8 -*-
+# Copyright 2013 Thomas Bechtold <thomasbechtold@jpberlin.de>
+# Copyright 2013 Deutsche Telekom AG
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from setuptools import setup, find_packages
 import os
 
@@ -23,13 +38,13 @@ setup(
     author_email="thomasbechtold@jpberlin.de",
     description="export apt package catalog",
     long_description=long_desc,
-    license="GPL-3",
+    license="Apache-2.0",
     keywords="apt dpkg debian ubuntu",
     url="https://github.com/toabctl/aptexport",
 
     classifiers=[
         "Development Status :: 4 - Beta",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "License :: OSI Approved :: Apache-2.0",
         "Programming Language :: Python",
     ],
 )
